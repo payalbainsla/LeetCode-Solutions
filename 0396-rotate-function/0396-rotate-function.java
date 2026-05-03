a842a9f672632585d23a026c9fbaac6c41d7a8a4
payalbainsla <bainslapayal377@gmail.com>
2026-05-03 20:54:54 +0530

    Time: 4 ms (93.42%), Space: 90.6 MB (47.19%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0, f0 = 0;
+
+        // Calculate sum(nums) and F(0)
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f0 += (long) i * nums[i];
+        }
+
+        long max = f0;
+        long fk = f0;
+
+        // Use recurrence relation to compute F(k)
+        for (int k = 1; k < n; k++) {
+            fk = fk + sum - (long) n * nums[n - k];
+            max = Math.max(max, fk);
+        }
+
+        return (int) max;
+    }
+}
