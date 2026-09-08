53a82b559e932e1a69e301055693d252c6401e68
Payal bainsla <bainslapayal377@gmail.com>
2026-09-08 20:47:10 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,6 +1,23 @@
 class Solution {
     public int countCommas(int n) {
- return n < 1000 ? 0 : n-999;
-    
+        long total = 0;
+        long low = 1;
+        long high = 9;
+        int digits = 1;
+
+        while (low <= n) {
+            long rangeHigh = Math.min(high, n);
+            long count = rangeHigh - low + 1;
+            int commasPerNumber = (digits - 1) / 3;
+
+            total += count * commasPerNumber;
+
+            // agla digit-length range
+            low = high + 1;
+            high = high * 10 + 9;
+            digits++;
+        }
+
+        return (int) total;
     }
 }
